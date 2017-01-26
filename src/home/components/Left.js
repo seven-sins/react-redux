0303713ed58f16738a41c044ae67b7b4fd88ac46
seven sins <ersanyu@163.com>
2017-01-26 12:01:16 +0800

    fix

--- src/home/components/Left.js
+++ src/home/components/Left.js
@@ -6,12 +6,28 @@ import { Link, IndexLink } from 'react-router';
 import './Left.less';
 
 class Left extends Component{
+    constructor(props, context){
+        super(props, context);
+        this.state = {
+            load: false
+        }
+    }
+    shouldComponentUpdate = (nextProps, nextState) => {
+        if(!this.state.load){
+            this.setState({
+                load: true
+            });
+            return true;
+        }else{
+            return false;
+        }
+    };
     render = () =>{
         return(
             <div className='left-container'>
                 <ul className='nav'>
-                    <li><i className='fa fa-file-text-o'></i><Link to="/">首页</Link></li>
-                    <li><i className='fa fa-file-text-o'></i><Link to="/user">用户管理</Link></li>
+                    <li><i className='fa fa-file-text-o'> </i><Link to="/">首页</Link></li>
+                    <li><i className='fa fa-file-text-o'> </i><Link to="/user">用户管理</Link></li>
                 </ul>
             </div>
         )
--- src/home/components/Top.js
+++ src/home/components/Top.js
@@ -5,6 +5,22 @@ import React, { Component, PropTypes } from 'react';
 import './Top.less';
 
 class Top extends Component{
+    constructor(props, context){
+        super(props, context);
+        this.state = {
+            load: false
+        }
+    }
+    shouldComponentUpdate = (nextProps, nextState) => {
+        if(!this.state.load){
+            this.setState({
+                load: true
+            });
+            return true;
+        }else{
+            return false;
+        }
+    };
     render = () =>{
         return(
             <div className='top-container'>
